b3aafef2306d4fdb1e583bbf84f7a21ad8c6073e
Morphes <92577219+Morphess@users.noreply.github.com>
2026-04-06 09:43:21 +0300

    Add Main class to demonstrate graph usage

---
+++ src/app/Main.java
@@ -0,0 +1,39 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+
+        System.out.println("Graph after adding vertices and edges: ");
+        graph.printGraph();
+        System.out.println();
+
+        System.out.println("Has vertex 3: " + graph.hasVertex(3));
+        System.out.println("Has vertex 5: " + graph.hasVertex(5));
+        System.out.println();
+
+        System.out.println("Has edge 1-2: " + graph.hasEdge(1, 2));
+        System.out.println("Has vertex 5: " + graph.hasEdge(3, 4));
+        System.out.println();
+
+        graph.removeEdge(1, 2);
+        System.out.println("Graph after removing edge 1-2");
+        graph.printGraph();
+        System.out.println();
+
+        graph.removeVertex(3);
+        System.out.println("Graph after removing vertex 3:");
+        graph.printGraph();
+
+
+    }
+}
